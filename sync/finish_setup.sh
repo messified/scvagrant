635ea8b0cb5338091fd19b153cb3035917fb5163
Jesse Reese <jesse.c.reese@gmail.com>
2017-08-01 16:10:19 -0700

    added docker ce install to finish_setup.sh

--- sync/finish_setup.sh
+++ sync/finish_setup.sh
@@ -2,5 +2,6 @@
 
 sh update_node.sh
 sh install_development_dependencies.sh
+sh install_docker.sh
 
 exit
--- sync/install_docker.sh
+++ sync/install_docker.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-sudo apt-get remove docker docker-engine docker.io
-sudo apt-get install \
+echo 'Installing Docker CE'
+sudo apt-get remove -y docker docker-engine docker.io
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -9,4 +10,14 @@ sudo apt-get install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+echo 'Docker installed, run command below to verify (optional)'
+echo 'sudo docker run hello-world'
+echo 'Installation Complete!'
