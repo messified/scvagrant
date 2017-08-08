23a4f4513a4ccd1ee6f1d2d7d5fbc1d52db661e3
Jesse Reese <jesse.c.reese@gmail.com>
2017-08-08 14:38:04 -0700

    setup fix

--- puphpet/files/exec-once/setup.sh
+++ puphpet/files/exec-once/setup.sh
@@ -9,10 +9,10 @@ sudo npm install -g typescript
 #MongoDB
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
+sudo apt-get updates
 sudo apt-get install -y mongodb-org
 sudo cp /var/www/mongod.service /lib/systemd/system/
 
-sudo apt-get -y upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 
 exit
